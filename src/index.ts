7ab04a4dd78cdd53278a5d159db48dc5e150507e
Nils Kolvenbach <info@nils-kolvenbach.de>
2020-06-16 21:25:05 +0200

    Loading a list of local projects now does not throw an error if there are other subdirectories, not used for a project. Added a test for that.

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,51 @@ import Project from './project';
 
 export const project = Project;
 
-export async function projects(): Promise<Project[]> {
-  const projects: Promise<Project>[] = [];
-  // Get all ID's from the projects folder
-  const dirent = await Fs.promises.readdir(Util.pathTo.projects, { withFileTypes: true });
-  dirent.filter((dirent) => {
-    return dirent.isDirectory();
-  }).map((possibleProjectDirectory) => {
-    // Try loading each project
-    try {
-      projects.push(new Project().load(possibleProjectDirectory.name));
-    } catch (error) {
-      console.log(`Unable to load possible project "${possibleProjectDirectory.name}": ` + error);
-    }
-  });
-  return await Promise.all(projects);
-}
+export const projects = {
+  /**
+   * Returns a list of all local projects
+   */
+  local: async (): Promise<Project[]> => {
+    const projects: Promise<Project | Error>[] = [];
+    // Get all ID's from the projects folder
+    const dirent = await Fs.promises.readdir(Util.pathTo.projects, { withFileTypes: true });
+    dirent.filter((dirent) => {
+      return dirent.isDirectory();
+    }).map(async (possibleProjectDirectory) => {
+      // Here comes the trick:
+      // By using "then" and "catch" we are able to create an array of Project and Error types
+      // without throwing and stopping the later Promise.all() call prematurely
+      projects.push(new Project().load(possibleProjectDirectory.name).then((project) => {
+        return project;
+      }).catch((error) => {
+        console.log('Unable to load possible project: ' + error);
+        // Because the error parameter could be anything, 
+        // we need to specifically call an Error 
+        return new Error(error);
+      }));
+    });
+
+    // Resolve all promises
+    // Here we do not expect any error to fail the call to Promise.all()
+    // because we catched it earlier and returning an Error type instead of throwing it
+    const resolvedProjects = await Promise.all(projects);
+
+    // This way we can easily filter out any Error types
+    // and are able to return only initialized projects 
+    // that did not throw an error.
+    // Note that we also need to use a User-Defined Type Guard here,
+    // because otherwise TS does not recognize we are filtering the errors out
+    //                                        >       |        <
+    return resolvedProjects.filter((project): project is Project => {
+      return project instanceof Error !== true;
+    });
+  },
+  /**
+   * Returns a list of all remote projects
+   * @todo finish once elek.io API is available
+   */
+  remote: async (): Promise<Project[]> => {
+    const projects: Project[] = [];
+    return await Promise.all(projects);
+  }
+};
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,4 @@
+import Path from 'path';
 import * as Elek from '../src/index';
 import * as Util from '../src/util';
 import { Signature } from 'nodegit';
@@ -9,7 +10,13 @@ beforeAll(async () => {
 
 test('creating new project "My first project"', async () => {
   await new Elek.project().create('My first project', Signature.now('John Doe', 'john.doe@domain.com'));
-  const projects = await Elek.projects();
+  const projects = await Elek.projects.local();
   expect(projects[0]).not.toBeUndefined();
   expect(projects[0].name).toBe('My first project');
+});
+
+test('subdirectories inside the projects directory that are not used as a project, should not produce errors', async () => {
+  await Util.mkdir(Path.join(Util.pathTo.projects, 'some-other-folder'));
+  const projects = await Elek.projects.local();
+  expect(projects.length).toBe(1);
 });
