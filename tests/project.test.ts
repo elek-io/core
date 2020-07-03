e2af384fa82ae38288fd1121f29e78c413e9353f
Nils Kolvenbach <info@nils-kolvenbach.de>
2020-07-03 19:59:03 +0200

    Improved project tests

--- tests/project.test.ts
+++ tests/project.test.ts
@@ -1,51 +1,43 @@
 import Fs from 'fs-extra';
 import Elek from '../src/index';
 import { Signature } from 'nodegit';
+import Project from '../src/project';
 
 const signature = Signature.now('John Doe', 'john.doe@domain.com');
 
-let firstProjectId: string;
+let project: Project;
 
 beforeAll(async () => {
   await Elek.init();
+  project = await new Elek.project().create('My first project', signature);
 });
 
 afterAll(async () => {
-  await (await new Elek.project().load(firstProjectId)).delete();
+  await project.delete();
 });
 
 describe('project module', () => {
 
-  it('should create a new project "My first project"', async () => {
-    const project = await new Elek.project().create('My first project', signature);
-    firstProjectId = project.id;
-    expect(project.config.name).toBe('My first project');
-  });
-
   it('should be able to load the existing project "My first project"', async () => {
-    const project = await new Elek.project().load(firstProjectId);
-    expect(project.config.name).toBe('My first project');
+    const loadedProject = await new Elek.project().load(project.id);
+    expect(loadedProject.config.name).toBe('My first project');
   });
 
   it('should not be able to reload an already initialized project', async () => {
-    const project = await new Elek.project().load(firstProjectId);
     await expect(project.load(project.id)).rejects.toThrowError();
   });
 
   it('should be able to save changes to disk', async () => {
-    const project = await new Elek.project().load(firstProjectId);
     project.config.name = 'The first project';
     await project.save(signature);
     // Test the config in memory
     expect(project.config.name).toBe('The first project');
-    // Load the same project again but now from disk
-    const projectAgain = await new Elek.project().load(firstProjectId);
-    // And check again if the changes are still present
-    expect(projectAgain.config.name).toBe('The first project');
+    // And check again if the changes are also present on disk
+    const projectConfig = await Elek.util.read.project(project.id);
+    expect(projectConfig.name).toBe('The first project');
   });
 
   it('should be able to create new pages', async () => {
-    const project = await new Elek.project().load(firstProjectId);
     await project.page.create(signature, {
       name: 'Another page',
       slug: 'another-page',
@@ -62,7 +54,6 @@ describe('project module', () => {
   });
 
   it('should be able to find a page', async () => {
-    const project = await new Elek.project().load(firstProjectId);
     const firstPage = await project.page.find('id', project.pages[0].id);
     expect(firstPage).toBeDefined();
     if (firstPage) {
