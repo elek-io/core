c09a03f7b90be12cb297b3692f947e3527830fe1
Nils Kolvenbach <info@nils-kolvenbach.de>
2020-07-16 20:16:18 +0200

    Added empty snapshot class for later implementation

---
+++ src/snapshot.ts
@@ -0,0 +1,10 @@
+// import Fs from 'fs-extra';
+// import Path from 'path';
+// import Util from './util';
+
+/**
+ * References a point in time of given project
+ * 
+ * Internally handled by git via tags
+ */
+export default class Snapshot {}
