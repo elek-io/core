79fb4fcd141e7641ff67368ae3b792616117fc42
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-08-10 11:42:41 +0200

    Fix: same export for Node and Browser

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -5,4 +5,4 @@ export * from './util/shared.js';
 
 // Also export the type of ElekIoCore but not the code itself
 // This is needed to be able to import and use the type inside the preload script
-export type { default as ElekIoCore } from './index.node.js';
+export type { default } from './index.node.js';
