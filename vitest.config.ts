69b23338ace827021c10612be7f69cb0e1b3cdf0
Nils Kolvenbach <info@nils-kolvenbach.de>
2026-04-13 12:54:54 +0200

    Added global vitest setup to cleanup left behind projects and generated clients of previously failed tests.

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     // are getting the total number of Projects, Assets etc.
     // which fails depending on the timing they are run (race condition).
     fileParallelism: false,
+    globalSetup: './src/test/globalSetup.ts',
     coverage: {
       reporter: ['text', 'lcov'],
     },
